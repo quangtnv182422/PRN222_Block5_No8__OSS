6151615c1b19d76430dfb4a58c4783f6ef35173a
PhanKien <phankienk51@gmail.com>
2025-04-23 22:51:00 +0700

    Reapply "rating"
    This reverts commit fd836e01941b60e0c058d047a6882f00110f6383.

---
+++ OSS_Main/wwwroot/js/rating.js
@@ -0,0 +1,18 @@
+﻿  const stars = document.querySelectorAll('.star-icon');
+    const ratingInput = document.getElementById('ratingInput');
+
+    stars.forEach(star => {
+        star.addEventListener('click', () => {
+            const selectedValue = parseInt(star.getAttribute('data-value'));
+            ratingInput.value = selectedValue;
+
+            stars.forEach(s => {
+                const starValue = parseInt(s.getAttribute('data-value'));
+                if (starValue <= selectedValue) {
+                    s.classList.add('selected');
+                } else {
+                    s.classList.remove('selected');
+                }
+            });
+        });
+    });
